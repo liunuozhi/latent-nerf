31f5dbf30e14475536e4a4a25f2aa74687f96cec
liunuozhi <liunuozhi1995@gmail.com>
2023-02-14 12:20:57 +0800

    Pass trainer to GUI

--- gui.py
+++ gui.py
@@ -1,8 +1,11 @@
-import pyrallis
-import torch
 from src.latent_nerf.configs.train_config import TrainConfig
 from src.latent_nerf.training.trainer import Trainer
 from src.latent_nerf.models.network_grid import NeRFNetwork
+
+import pyrallis
+import torch
+import dearpygui.dearpygui as dpg
+import numpy as np
 
 
 class GUITrainer(Trainer):
@@ -23,12 +26,157 @@ class GUITrainer(Trainer):
         return None
 
 
+class OrbitCamera:
+    def __init__(self, K, img_wh, r):
+        self.K = K
+        self.W, self.H = img_wh
+        self.radius = r
+        self.center = np.zeros(3)
+        self.rot = np.eye(3)
+
+    @property
+    def pose(self):
+        # first move camera to radius
+        res = np.eye(4)
+        res[2, 3] -= self.radius
+        # rotate
+        rot = np.eye(4)
+        rot[:3, :3] = self.rot
+        res = rot @ res
+        # translate
+        res[:3, 3] -= self.center
+        return res
+
+    def orbit(self, dx, dy):
+        rotvec_x = self.rot[:, 1] * np.radians(0.05 * dx)
+        rotvec_y = self.rot[:, 0] * np.radians(-0.05 * dy)
+        self.rot = R.from_rotvec(rotvec_y).as_matrix() @ \
+                   R.from_rotvec(rotvec_x).as_matrix() @ \
+                   self.rot
+
+    def scale(self, delta):
+        self.radius *= 1.1 ** (-delta)
+
+    def pan(self, dx, dy, dz=0):
+        self.center += 1e-4 * self.rot @ np.array([dx, dy, dz])
+
+
+class NGPGUI:
+    def __init__(self, trainer):
+        img_wh = [1000, 1000]
+        K = 1
+        radius = 2.5
+
+        self.W, self.H = img_wh
+        self.cam = OrbitCamera(K, img_wh, r=radius)
+        self.render_buffer = np.ones((self.W, self.H, 3), dtype=np.float32)
+
+        self.dt = 0
+        self.mean_sample = 0
+        self.img_mode = 0
+
+        self.trainer = trainer
+
+        self.register_dpg()
+
+    def render_cam(self, cam):
+        rgb = np.zeros((1000, 1000, 3))
+        depth = np.zeros((1000, 1000))
+
+        if self.img_mode == 0:
+            return rgb
+        elif self.img_mode == 1:
+            return depth.astype(np.float32) / 255.
+
+    def register_dpg(self):
+        dpg.create_context()
+        dpg.create_viewport(title="ngp_pl", width=self.W, height=self.H, resizable=False)
+
+        ## register texture ##
+        with dpg.texture_registry(show=False):
+            dpg.add_raw_texture(
+                self.W,
+                self.H,
+                self.render_buffer,
+                format=dpg.mvFormat_Float_rgb,
+                tag="_texture")
+
+        ## register window ##
+        with dpg.window(tag="_primary_window", width=self.W, height=self.H):
+            dpg.add_image("_texture")
+        dpg.set_primary_window("_primary_window", True)
+
+        def callback_depth(sender, app_data):
+            self.img_mode = 1 - self.img_mode
+
+        ## control window ##
+        with dpg.window(label="Control", tag="_control_window", width=200, height=150):
+            dpg.add_slider_float(label="exposure", default_value=0.2,
+                                 min_value=1 / 60, max_value=32, tag="_exposure")
+            dpg.add_button(label="show depth", tag="_button_depth",
+                           callback=callback_depth)
+            dpg.add_separator()
+            dpg.add_text('no data', tag="_log_time")
+            dpg.add_text('no data', tag="_samples_per_ray")
+
+        ## register camera handler ##
+        def callback_camera_drag_rotate(sender, app_data):
+            if not dpg.is_item_focused("_primary_window"):
+                return
+            self.cam.orbit(app_data[1], app_data[2])
+
+        def callback_camera_wheel_scale(sender, app_data):
+            if not dpg.is_item_focused("_primary_window"):
+                return
+            self.cam.scale(app_data)
+
+        def callback_camera_drag_pan(sender, app_data):
+            if not dpg.is_item_focused("_primary_window"):
+                return
+            self.cam.pan(app_data[1], app_data[2])
+
+        with dpg.handler_registry():
+            dpg.add_mouse_drag_handler(
+                button=dpg.mvMouseButton_Left, callback=callback_camera_drag_rotate
+            )
+            dpg.add_mouse_wheel_handler(callback=callback_camera_wheel_scale)
+            dpg.add_mouse_drag_handler(
+                button=dpg.mvMouseButton_Middle, callback=callback_camera_drag_pan
+            )
+
+        ## Avoid scroll bar in the window ##
+        with dpg.theme() as theme_no_padding:
+            with dpg.theme_component(dpg.mvAll):
+                dpg.add_theme_style(
+                    dpg.mvStyleVar_WindowPadding, 0, 0, category=dpg.mvThemeCat_Core
+                )
+                dpg.add_theme_style(
+                    dpg.mvStyleVar_FramePadding, 0, 0, category=dpg.mvThemeCat_Core
+                )
+                dpg.add_theme_style(
+                    dpg.mvStyleVar_CellPadding, 0, 0, category=dpg.mvThemeCat_Core
+                )
+        dpg.bind_item_theme("_primary_window", theme_no_padding)
+
+        ## Launch the gui ##
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+
+    def render(self):
+        while dpg.is_dearpygui_running():
+            dpg.set_value("_texture", self.render_cam(self.cam))
+            # dpg.set_value("_log_time", f'Render time: {1000*self.dt:.2f} ms')
+            # dpg.set_value("_samples_per_ray", f'Samples/ray: {self.mean_samples:.2f}')
+            dpg.render_dearpygui_frame()
+
+
 @pyrallis.wrap()
 def main(cfg: TrainConfig):
-    trainer = GUITrainer(cfg)
-    trainer.full_eval()
-    return 0
+    gui_trainer = GUITrainer(cfg)
+    return gui_trainer
 
 
 if __name__ == '__main__':
-    main()
+    trainer = main()
+    NGPGUI(trainer).render()
+    dpg.destroy_context()
